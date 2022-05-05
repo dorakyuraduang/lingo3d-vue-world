107ac2d101f1fef510557b633240133b0090154c
wangcm1 <wangcm1@getui.com>
2022-05-05 11:36:21 +0800

    refactor(model-loading): 调整加载模型的顺序，优化加载体验

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,6 +1,14 @@
 <template>
-  <el-form ref="ruleFormRef" :rules="rules" hide-required-asterisk class="join" size="large" label-width="auto"
-    label-position="top" :model="formData">
+  <el-form
+    ref="ruleFormRef"
+    :rules="rules"
+    hide-required-asterisk
+    class="join"
+    size="large"
+    label-width="auto"
+    label-position="top"
+    :model="formData"
+  >
     <div class="title">欢迎加入游戏</div>
     <el-form-item label="输入名字" prop="name">
       <el-input v-model="formData.name" minlength="2" maxlength="16" placeholder="请输入你的名字" />
@@ -9,57 +17,62 @@
       <role-pick v-model:role="formData.role" />
     </el-form-item>
     <el-form-item class="join-btn">
-      <el-button type="primary"  class="bg-blue-400" @click="submitForm(ruleFormRef)">加入游戏</el-button>
+      <el-button type="primary" class="bg-blue-400" @click="submitForm(ruleFormRef)">
+        加入游戏
+      </el-button>
     </el-form-item>
   </el-form>
 </template>
 <script lang="ts" setup>
-import RolePick from '@/components/RolePick.vue'
-import { reactive, ref } from 'vue'
-import useMessage from '@/hooks/useMessage';
-import { FormInstance, FormRules, ElMessage } from 'element-plus'
-import socket from '@/utils/socket';
-import useUserStore from '@/store/modules/player'
-const ruleFormRef = ref<FormInstance>()
-const userStore = useUserStore()
+import RolePick from "@/components/RolePick.vue";
+import { reactive, ref, defineEmits } from "vue";
+import useMessage from "@/hooks/useMessage";
+import { FormInstance, FormRules, ElMessage } from "element-plus";
+import socket from "@/utils/socket";
+import useUserStore from "@/store/modules/player";
+const ruleFormRef = ref<FormInstance>();
+const userStore = useUserStore();
 const formData = reactive({
-  name: '',
-  role: ""
-})
+  name: "",
+  role: "",
+});
+
 const rules = reactive<FormRules>({
   name: [
-    { required: true, message: '请输入名字', trigger: 'blur' },
-    { min: 2, max: 16, message: '名字长度在2到16位', trigger: 'blur' },
+    { required: true, message: "请输入名字", trigger: "blur" },
+    { min: 2, max: 16, message: "名字长度在2到16位", trigger: "blur" },
   ],
   role: [
     {
       required: true,
-      message: '请选择人物',
-      trigger: 'blur',
+      message: "请选择人物",
+      trigger: "blur",
     },
-  ]
-})
+  ],
+});
+const emit = defineEmits(["select-role"]);
 const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return
+  if (!formEl) return;
   await formEl.validate((valid, fields) => {
     if (valid) {
-      socket.emit('join', formData)
+      socket.emit("join", formData);
+      emit("select-role", formData.role);
     } else {
-      useMessage('加入游戏失败')
+      useMessage("加入游戏失败");
     }
-  })
-}
-socket.on('join', ({ code, name, role, id }) => {
+  });
+};
+socket.on("join", ({ code, name, role, id }) => {
   if (code == 200) {
-    userStore.id = id
-    userStore.name = name
-    userStore.role = role
-    userStore.isJoin = true
-    useMessage('成功加入游戏')
+    userStore.id = id;
+    userStore.name = name;
+    userStore.role = role;
+    userStore.isJoin = true;
+    useMessage("成功加入游戏");
   } else {
-    useMessage('加入游戏失败')
+    useMessage("加入游戏失败");
   }
-})
+});
 </script>
 <style lang="less" scoped>
 .title {
@@ -81,10 +94,9 @@ socket.on('join', ({ code, name, role, id }) => {
   background-color: #fff;
 
   ::v-deep(&-btn) {
-    >div {
+    > div {
       justify-content: center;
     }
-
   }
 }
 </style>
